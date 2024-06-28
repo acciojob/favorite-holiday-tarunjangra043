34e26e93676473d85b7aa2b578e671c3babcf4a3
Tarun Jangra <111531292+tarunjangra043@users.noreply.github.com>
2024-06-28 21:40:58 +0530

    created a submission

--- script.js
+++
@@ -1,5 +0,0 @@
-//your JS code here. If required.
-cy.get('b').should('be.visible').then(($element) => {
-    // Log element information for debugging
-    cy.log(`Found element: ${$element.text()}`);
-});
